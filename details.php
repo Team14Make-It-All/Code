82784f9ba5dfd59383b16987717d9abad6921361
akitochu <akitochu@gmail.com>
2019-11-06 12:05:52 +0000

    Update details.php

--- details.php
+++ details.php
@@ -7,32 +7,74 @@ table {
   border-collapse: collapse;
   width: 100%;
 }
-
 body {
 	text-align: center;
 }
-
 td, th {
   border: 1px solid #000000;
   text-align: center;
   padding: 8px;
 }
-
 h2 {
   text-align: center;
   text-decoration: underline;
 }
-
 .todo {
   background-color: #ffcccb;	
 }
-
 .complete {
   background-color: #90ee90;
 }
 
+.boxed {
+	border: 1px solid black;
+	display: inline-block;
+	padding-top: 5px;
+	padding-bottom: 5px;
+	padding-left: 15px;
+	padding-right: 15px;
+}
+
 body {
   background-color: #dddddd;
+}
+
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content */
+.modal-content {
+  background-color: #fefefe;
+  margin: auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+/* The Close Button */
+.close {
+  color: #aaaaaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
 }
 
 
@@ -40,11 +82,22 @@ body {
 </head>
 <body>
 
-	<table>
-		<tr><th>Job Name: </th></tr>
-		<tr><td>Job ID:</td></tr>
-		<tr><td>Job Deadline:</td></tr>
-	</table>
+	<div class="boxed">
+		Job Name: Removing questionable stains from office computers 
+	</div>
+	<br>
+	<div class="boxed">
+		Job ID: G12055
+	</div>
+	<br>
+	<div class="boxed">	
+		Job Deadline: 25/12/2019
+	</div>
+	<br>
+	<button id="item" class="editbtn">Item Information</button>
+	<br>
+	<button id="product" class="editbtn">Product Information</button>
+	<br>
 	
 	
 	<textarea name="message" rows="10" cols="30">
@@ -54,9 +107,41 @@ body {
 	<br><br><br><br>
 
 <?php
-
-
 ?>
+
+	<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <h>Serial Number:     </h><textarea name="message" rows="1" cols="10">94-05-02</textarea><br>
+	<h>Problem Date:      </h><textarea name="message" rows="1" cols="10">01/01/2004</textarea><br>
+	<h>Solution Available:</h><textarea name="message" rows="1" cols="10">True</textarea><br>
+	<h>Previous Item Problems: </h><br><textarea name="message" rows="5" cols="40"></textarea><br>
+	<button id="return3" class="editbtn">Discard</button>
+	============
+	<button id="return4" class="editbtn">Save</button>
+  </div>
+
+</div>
+
+<div id="myModal2" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <h>Product Name:     </h><textarea name="message" rows="1" cols="10">HP5660</textarea><br>
+	<h>Manufacturer:         </h><textarea name="message" rows="1" cols="10">HP</textarea><br>
+	<h>Previous Product Problems: </h><br><textarea name="message" rows="5" cols="40"></textarea><br>
+	<button id="return5" class="editbtn">Discard</button>
+	============
+	<button id="return6" class="editbtn">Save</button>
+  </div>
+
+</div>
+
+
 
 <button id="return1" class="editbtn">Discard</button>
 ============
@@ -66,6 +151,32 @@ body {
 var btn = document.getElementById('return1');
 btn.addEventListener('click', function() {
   document.location.href = 'technician.php';
+});
+var modal = document.getElementById("myModal");
+var btn = document.getElementById('item');
+btn.addEventListener('click', function() {
+  modal.style.display = "block";
+});
+var modal2 = document.getElementById("myModal2");
+var btn = document.getElementById('product');
+btn.addEventListener('click', function() {
+  modal2.style.display = "block";
+});
+var btn = document.getElementById('return3');
+btn.addEventListener('click', function() {
+  modal.style.display = "none";
+});
+var btn = document.getElementById('return4');
+btn.addEventListener('click', function() {
+  modal.style.display = "none";
+});
+var btn = document.getElementById('return5');
+btn.addEventListener('click', function() {
+  modal2.style.display = "none";
+});
+var btn = document.getElementById('return6');
+btn.addEventListener('click', function() {
+  modal2.style.display = "none";
 });
 var btn = document.getElementById('return2');
 btn.addEventListener('click', function() {
