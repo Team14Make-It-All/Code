661dd3c50f785d700f591c6380c0c07446c5f985
emandrael <57156170+emandrael@users.noreply.github.com>
2019-11-07 14:10:23 +0000

    Update technician.php

--- technician.php
+++ technician.php
@@ -38,73 +38,73 @@ body {
 
 <?php
 
-echo "<h2>To Do List</h2>";
+echo "<h2>Active Problems</h2>";
 echo "<br>";
 ?>
 	<div class="todo">
 <?php
 echo "<table>";
 echo   "<tr>";
-echo    "<th>Job Name</th>";
-echo     "<th>Job ID</th>";
-echo     "<th>Job Deadline</th>";
+echo    "<th>Problem</th>";
+echo     "<th>Problem ID</th>";
+echo     "<th>Problem Start</th>";
 echo 	"<th>Job Completion</th>";
 echo 	"<th></th>";
 echo   "</tr>";
 echo   "<tr>";
-echo     "<td>Removing questionable stains from office computers</td>";
-echo     "<td>G12055</td>";
+echo     "<td>Paper Stuck in Printer</td>";
+echo     "<td>0</td>";
 echo     "<td>25/12/2019</td>";
 ?>
 <td><button class="editbtn">Complete</button></td>
-<td><button id="detail1" class="editbtn">Job Details</button></td>
+<td><button id="detail1" class="editbtn">Job Details</button><button>Relocate Job</button></td>
 
 
 <?php
 echo   "</tr>";
 echo   "<tr>";
-echo     "<td>Finding out the half-life 3 release date</td>";
-echo    "<td>N3V312</td>";
+echo     "<td>Excel vlookup malfunction</td>";
+echo    "<td>1</td>";
 echo     "<td>1/1/3000</td>";
 ?>
 <td><button class="editbtn">Complete</button></td>
-<td><button id="detail2" class="editbtn">Job Details</button></td>
+<td><button id="detail2" class="editbtn">Job Details</button><button>Relocate Job</button></td>
 <?php
 echo   "</tr>";
 echo   "<tr>";
 echo     "<td>Figuring out how bitcoin works</td>";
-echo     "<td>G58945</td>";
+echo     "<td>2</td>";
 echo     "<td>2//2/2022</td>";
 ?>
 <td><button class="editbtn">Complete</button></td>
-<td><button id="detail3" class="editbtn">Job Details</button></td>
+<td><button id="detail3" class="editbtn">Job Details</button><button>Relocate Job</button> </td>
 <?php
 echo   "</tr>";
 echo   "<tr>";
-echo     "<td>GPU malfunction</td>";
-echo     "<td>G585648</td>";
+echo     "<td>GPU malfunction in Graphics Stations</td>";
+echo     "<td>3</td>";
 echo     "<td>8/9/2020</td>";
 ?>
 <td><button class="editbtn">Complete</button></td>
-<td><button id="detail4" class="editbtn">Job Details</button></td>
+<td><button id="detail4" class="editbtn">Job Details</button><button>Relocate Job</button></td>
 <?php
 echo   "</tr>";
 echo   "<tr>";
-echo     "<td>Renewing room 1 office software</td>";
-echo     "<td>S845692</td>";
+echo     "<td>Renewing the Room 1 office software</td>";
+echo     "<td>4</td>";
 echo     "<td>9/12/2019</td>";
 ?>
 <td><button class="editbtn">Complete</button></td>
-<td><button id="detail5" class="editbtn">Job Details</button></td>
+<td><button id="detail5" class="editbtn">Job Details</button><button>Relocate Job</button></td>
 <?php
 echo   "</tr>";
 echo   "<tr>";
-echo     "<td>Replacing all printers, for circuit washing machines</td>";
-echo     "<td>H482168</td>";
+echo     "<td>Replacing all printers, for state of the art type writers, where needed</td>";
+echo     "<td>5</td>";
 echo     "<td>8/7/2019</td>";
 ?>
 <td><button class="editbtn">Complete</button></td>
-<td><button id="detail6" class="editbtn">Job Details</button></td>
+<td><button id="detail6" class="editbtn">Job Details</button><button>Relocate Job</button></td>
 <?php
 echo   "</tr>";
 echo "</table>";
@@ -112,38 +112,51 @@ echo "</div>";
 
 echo "<br><br><br>";
 ?>
-<h2>Completed Jobs  <button class="editbtn">Clear All</button></h2>
+<h2>Completed Problems <button class="editbtn">Clear All</button></h2>
 <?php
 echo "<br>";
 ?>
 <div class="complete">
 <?php
-echo "<h4>";
+
 echo "<table>";
 echo   "<tr>";
-echo     "<th>Job Name</th>";
-echo     "<th>Job ID</th>";
-echo     "<th>Job Deadline</th>";
+echo     "<th>Problem</th>";
+echo     "<th>Problem ID</th>";
+echo     "<th>Problem Start</th>";
+echo     "<th>Date of Completion</th>";
 echo 	"<th></th>";
 echo   "</tr>";
 echo   "<tr>";
 echo     "<td>Fixing a piece of code that was messing with the washing machines</td>";
-echo     "<td>SH542691</td>";
+echo     "<td>8</td>";
+echo     "<td>2/10/2019</td>";
 echo     "<td>2/10/2019</td>";
 ?>
-<td><button class="editbtn">Undo Completion</button></td>
+<td><button class="editbtn">Undo Completion</button><button id="detail7" class="editbtn">Job Details</button></td>
 <?php
 echo   "</tr>";
 echo   "<tr>";
 echo     "<td>Fixing the computers at Centro comercial Moctezuma</td>";
-echo     "<td>H845845</td>";
-echo     "<td>H482184</td>";
+echo     "<td>9</td>";
+echo     "<td>12/25/1990</td>";
+echo     "<td>2/10/2019</td>";
 ?>
-<td><button class="editbtn">Undo Completion</button></td>
+<td><button class="editbtn">Undo Completion</button><button id="detail8" class="editbtn">Job Details</button></td>
+
+<?php
+echo   "</tr>";
+echo   "<tr>";
+echo     "<td>Head Office's Desktop in a neverending restart loop</td>";
+echo     "<td>10</td>";
+echo     "<td>12/25/1990</td>";
+echo     "<td>2/10/2019</td>";
+?>
+<td><button class="editbtn">Undo Completion</button><button id="detail9" class="editbtn">Job Details</button></td>
+
 <?php
 echo   "</tr>";
 echo "</table>";
-echo "</h4>";
 echo "</div>";
 
 ?>
@@ -172,6 +185,19 @@ btn.addEventListener('click', function() {
 var btn = document.getElementById('detail6');
 btn.addEventListener('click', function() {
   document.location.href = 'details.php';
+});
+
+var btn = document.getElementById('detail7');
+btn.addEventListener('click', function() {
+  document.location.href = 'detailsSolutions.php';
+});
+var btn = document.getElementById('detail8');
+btn.addEventListener('click', function() {
+  document.location.href = 'detailsSolutions.php';
+});
+var btn = document.getElementById('detail9');
+btn.addEventListener('click', function() {
+  document.location.href = 'detailsSolutions.php';
 });
 </script>
 
